71df0e49fce89bb8e771528250db6f9f0d4a3ccc
Yusuke Kawamoto <novogrammer@gmail.com>
2023-12-14 17:21:54 +0900

    GitHub Pages対応

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Hello @lumaai/luma-web
 
+demo https://novogrammer.github.io/hello-lumaai-luma-web/
+
 ライブラリのリポジトリがprivateなのと、ライセンスが存在しないので、使えない。
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/hello-lumaai-luma-web/",
   plugins: [react()],
 })
